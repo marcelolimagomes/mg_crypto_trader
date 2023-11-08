a364b66e86e5b053ce117a65a14fff8b9810059e
Marcelo Lima Gomes <marcelolimagomes@gmail.com>
2023-11-08 20:34:51 -0300

    Implements option to feature-selection and combine-feature-list

---
+++ show_ledger.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+while true; do
+  # Your command goes here
+  #  echo 'OPERATION | SYMBOL | INTERVAL | PnL | BALANCE | DATE OPERATION'
+  # sqlite3 db/mg_trader.db 'SELECT operation, symbol, interval, round(pnl, 2), round(balance,2), strftime("%Y-%m-%d %H:%M:%S", "created_at") FROM ledger'
+  # echo ''
+
+  echo 'SYMBOL | INTERVAL | TOTAL PnL | NUMBER SELLs'
+  sqlite3 db/mg_trader.db 'SELECT symbol, interval, sum(round(pnl, 2)), count(1) FROM ledger WHERE operation = "SELL" GROUP BY symbol, interval'
+  echo ''
+
+  echo 'SYMBOL | TOTAL PnL | NUMBER SELLs'
+  sqlite3 db/mg_trader.db 'SELECT symbol, sum(round(pnl, 2)), count(1) FROM ledger WHERE operation = "SELL" GROUP BY symbol'
+  echo ''
+
+  echo 'OPERATION | TOTAL PnL | NUMBER OPERATIONS'
+  sqlite3 db/mg_trader.db 'SELECT operation, sum(round(pnl, 2)), count(1) FROM ledger GROUP BY operation'
+  echo ''
+
+  echo 'TOTAL OPERATIONS IN BUY'
+  sqlite3 db/mg_trader.db 'SELECT (SELECT count(1) FROM ledger WHERE operation = "BUY" GROUP BY operation) - (SELECT count(1) FROM ledger WHERE operation = "SELL" GROUP BY operation)'
+
+  echo $(date)
+
+  # Sleep for 5 seconds
+  sleep 20
+done
+
+
+
+
